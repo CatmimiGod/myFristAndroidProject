2f7b51e71d76063bd4d9be2c29c582eb6bb0a254
CatmimiGod <catmimishen@qq.com>
2017-08-01 11:39:30 +0800

    增加导航条2

--- app/src/main/java/com/example/catmi/myfristandroidproject/MainActivity.java
+++ app/src/main/java/com/example/catmi/myfristandroidproject/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText("setting");
+                    return true;
             }
             return false;
         }
